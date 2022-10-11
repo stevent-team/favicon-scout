e23920dca2fcc703eef03996bd2b4231c673602e
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-10-12 00:07:53 +1100

    Extract logic into functions

--- src/favicon.ts
+++ src/favicon.ts
@@ -7,19 +7,36 @@ import { parse } from 'node-html-parser'
 import fallback from './fallback'
 
 // Try and find the link to an icon from a page's <link> tags
-const getUrlFromLink = async (url: URL): Promise<URL | undefined> => {
-  const pageRes = await fetch(url.href)
-  if (!pageRes?.ok) return
+const getUrlFromLink = async (url: URL): Promise<URL> => {
+  const res = await fetch(url.href)
+  if (!res?.ok) {
+    throw new Error('Cannot fetch webpage')
+  }
 
-  const pageXml = await pageRes.text()
-  const pageRoot = parse(pageXml)
-  const links = pageRoot.querySelectorAll('link[rel*="icon"]')
+  const page = parse(await res.text())
+  const links = page.querySelectorAll('link[rel*="icon"]')
     .filter(link => link.attributes?.href && !/prefers-color-scheme: *dark/.test(link.attributes?.media))
     .sort((a, b) => parseInt(a.attributes.sizes?.split('x')?.[0]) - parseInt(b.attributes.sizes?.split('x')?.[0]))
-    .map(link => new URL(link.attributes.href, url))
-  if (links.length > 0) {
-    return links.reverse()[0]
+    .map(link => new URL(link.attributes.href, new URL(res.url).origin))
+  if (links.length > 0) return links.reverse()[0]
+
+  throw new Error('No links found')
+}
+
+// Try and fetch the favicon from the url
+const fetchImage = async (url: URL): Promise<Sharp> => {
+  const res = await fetch(url)
+  if (!res?.ok) {
+    throw new Error('Cannot fetch favicon')
   }
+
+  const buffer = await res.buffer()
+  // If file extension is ico, return largest
+  if (url.pathname.split('/').reverse()[0].split('.').reverse()[0] === 'ico') {
+    return (ico.sharpsFromIco(buffer, undefined, true)
+      .sort((a, b) => (b.width ?? 0) - (a.width ?? 0))[0] as { image: Sharp }).image
+  }
+  return sharp(buffer)
 }
 
 const getFavicon = async (req: FastifyRequest, res: FastifyReply) => {
@@ -31,39 +48,20 @@ const getFavicon = async (req: FastifyRequest, res: FastifyReply) => {
   // Create a new URL for the page
   const url = new URL(path.startsWith('http') ? path : `https://${path}`)
 
-  // Get the URL of the favicon
-  const faviconURL = await getUrlFromLink(url) ?? new URL('favicon.ico', url.origin)
+  // Get the URL of the favicon, default to favicon.ico
+  const imageUrl = await getUrlFromLink(url).catch(() => new URL('favicon.ico', url.origin))
 
+  // Fetch the image, or use a fallback if it fails
+  let image: Sharp = await fetchImage(imageUrl).catch(() => sharp(fallback(url.host)))
   res.type('image/webp')
-  let sharpFile: Sharp | undefined
-
-  // Get icon
-  const fileRes = await fetch(faviconURL).catch(req.log.debug)
-  if (fileRes?.ok) {
-    try {
-      const fileBuffer = await fileRes.buffer()
-      if (faviconURL.pathname.split('/').reverse()[0].split('.').reverse()[0] === 'ico') {
-        sharpFile = (ico.sharpsFromIco(fileBuffer, undefined, true)
-          .sort((a, b) => (b.width ?? 0) - (a.width ?? 0))[0] as { image: Sharp }).image
-      } else {
-        sharpFile = sharp(fileBuffer)
-      }
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  if (!sharpFile) {
-    sharpFile = sharp(fallback(url.host))
-  }
 
   if (size) {
-    sharpFile = sharpFile.resize(parseInt(size), undefined, {
+    image = image.resize(parseInt(size), undefined, {
       fit: 'contain',
       background: { r: 0, g: 0, b: 0, alpha: 0 },
     })
   }
-  return sharpFile.webp().toBuffer()
+  return image.webp().toBuffer().catch(() => sharp(fallback(url.host)).webp().toBuffer())
 }
 
 export default getFavicon
