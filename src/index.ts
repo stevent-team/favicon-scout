07b2afdc13b3ff55f66bec9f28b3a1b5f9e86f37
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-10-14 02:14:55 +1100

    Set port in .env

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,23 @@ import getFavicon from './getFavicon'
 // Pull list of allowed origins from .env, default to * if not set
 dotenv.config()
 const origin: (string | RegExp)[] | string = process.env.ALLOWED_ORIGINS?.split(',').map(o => o.trim()).map(o => (o.startsWith('/') && o.endsWith('/')) ? new RegExp(o.slice(1,-1)) : o) ?? '*'
+const port: number = parseInt(process.env.PORT ?? '3000')
 
-const server = fastify()
+const app = fastify()
 
-server.register(cors, { origin })
+app.register(cors, { origin })
 
-server.get('/', () => ({ name, version }))
+app.get('/', () => ({ name, version }))
 
-server.get('/:path', getFavicon)
-server.get('/:path/:size', getFavicon)
+app.get('/:path', getFavicon)
+app.get('/:path/:size', getFavicon)
 
-server.listen({ port: 3002 }).then(() => {
-  console.log(`🧭 Favicon Scout v${version}`)
+app.listen({ port }).then(() => {
+  console.log(`🧭 Favicon Scout v${version} running at http://localhost:${port}`)
   console.log(`Allowing requests from: ${origin}`)
 }).catch(e => {
-  server.log.error(e)
+  app.log.error(e)
   process.exit(1)
 })
+
+export default app
